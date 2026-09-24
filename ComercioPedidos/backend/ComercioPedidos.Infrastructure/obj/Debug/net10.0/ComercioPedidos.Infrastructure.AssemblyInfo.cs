538c8acfd624aa89b5398f42e27c9fddc557f76e
HiroNk11 <nicolaseduardogiron@gmail.com>
2026-09-24 17:59:32 -0300

    Producto - Pedido - DetalleProducto Backend

--- ComercioPedidos/backend/ComercioPedidos.Infrastructure/obj/Debug/net10.0/ComercioPedidos.Infrastructure.AssemblyInfo.cs
+++ ComercioPedidos/backend/ComercioPedidos.Infrastructure/obj/Debug/net10.0/ComercioPedidos.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComercioPedidos.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1420d2ed69fd22f28f429b3e26ea2085b7423e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5228999f620cd90be3cb132caf56d99ed5c59724")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComercioPedidos.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComercioPedidos.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
